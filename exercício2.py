72a1e9465e6d70b9b87b4e943b0619cbfe72301f
Gustavo Sousa Aragão <guga.x64@gmail.com>
2016-02-04 16:48:43 -0300

    Update exercício2.py

--- exercício2.py
+++ exercício2.py
@@ -1,5 +1,5 @@
 a = "Alterar todas as letras do texto para maiúsculas."
-b = "ALTERAR TODAS AS LETRAS DO TEXTO PARA MINPUSCULA"
+b = "ALTERAR TODAS AS LETRAS DO TEXTO PARA MINÚSCULA"
 c = "Alterar somente a primeira letra de cada palavra para maiúscula"
 print(a.upper())
 print(b.lower())
