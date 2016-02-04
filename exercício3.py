07bcdf91c87642bdcbdaf3f494f6b2b3c245b409
Gustavo Sousa Aragão <guga.x64@gmail.com>
2016-02-04 12:01:05 -0300

    Create exercício3.py

---
+++ exercício3.py
@@ -0,0 +1,26 @@
+f = 200
+g = 250
+h = 8
+i = 7
+j = 800
+k = 10
+l = 2
+
+a = f + g
+b = f / h
+c = f * i
+d = j - k
+e = l **10
+
+print()
+print("Vamos Mostrar os Resultados das questões do EX3")
+print()
+print("%d É o resultado da letra A" %a)
+print()
+print("%d É o resultado da letra B" %b)
+print()
+print("%d É o resultado da letra C" %c)
+print()
+print("%d É o resultado da letra D" %d)
+print()
+print("%d É o resultado da letra E" %e)
