931a2a95c562d79e760c43e58bda16ff43d23a99
Gustavo Sousa Aragão <guga.x64@gmail.com>
2016-02-04 16:57:35 -0300

    Update exercício2.py

--- exercício2.py
+++ exercício2.py
@@ -1,6 +1,19 @@
-a = "Alterar todas as letras do texto para maiúsculas."
-b = "ALTERAR TODAS AS LETRAS DO TEXTO PARA MINÚSCULA"
-c = "Alterar somente a primeira letra de cada palavra para maiúscula"
+a = "Eu tenho um sonho." 
+b = "O sonho de ver meus filhos julgados por " 
+c = "sua personalidade, não pela cor de sua pele."
+d = "Martin Luther King Júnior"
+
 print(a.upper())
+print(b.upper())
+print(c.upper())
+print(d.upper())
+
+print(a.lower())
 print(b.lower())
+print(c.lower())
+print(d.lower())
+
+print(a.title())
+print(b.title())
 print(c.title())
+print(d.title())
